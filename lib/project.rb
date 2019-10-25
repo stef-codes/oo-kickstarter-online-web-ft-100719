7dffdea6cc4eadf3a97c53bc5663e8fbe2aaa694
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-25 19:03:05 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,7 +9,7 @@ class Project
   
   def add_backer(backer)
       @backers << backer
-      backer
+      backer.backed_projects << self
       # backer.back_project(self) unless backer.backed_projects.include?(self)
   end 
 end 
